05512a7136dd5bef5d75d471fba156fd8f59d42d
Stefan Jelner <stefan.jelner@gmx.de>
2024-03-27 15:57:29 +0100

    initial commit

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,147 @@
+<script context="module" lang="ts">
+	export enum ButtonIconPosition {
+		LEFT = 'left',
+		RIGHT = 'right'
+	}
+</script>
+
+<script lang="ts">
+	import classNames, { type Argument as ClassNamesArgument } from 'classnames';
+	// @ts-ignore
+	import { get_current_component } from 'svelte/internal';
+	import type { ActionReturn } from 'svelte/action';
+
+	// typing events
+	interface $$Events {
+		click: MouseEvent;
+		focus: FocusEvent;
+		blur: FocusEvent;
+		keydown: KeyboardEvent;
+		keyup: KeyboardEvent;
+		keypress: KeyboardEvent;
+		mouseover: MouseEvent;
+		mouseout: MouseEvent;
+		mouseenter: MouseEvent;
+		mouseleave: MouseEvent;
+	}
+
+	// typing slots
+	interface $$Slots {
+		icon: {};
+	}
+
+	// The input attributes
+	export let text: string = '';
+	export let disabled: boolean = false;
+	// little trick for using the reserved word "class"
+	let className: ClassNamesArgument = {};
+	export { className as class };
+	export let iconPosition: ButtonIconPosition = ButtonIconPosition.LEFT;
+	export let hideText: boolean = false;
+
+	// Normal variables
+	const component = get_current_component();
+
+	/**
+	 * Normalizes keydown event on button element
+	 *
+	 * Modern browsers transform enter or space on keydown into a click event. We prevent this unwanted behaviour
+	 * here.
+	 *
+	 * @param e the keyboard event
+	 */
+	function normalizeKeydownEvent(e: KeyboardEvent): void {
+		// Prevent click event
+		if (['Enter', 'NumpadEnter', 'Space'].indexOf(e.code) !== -1) {
+			e.preventDefault();
+		}
+
+		// Directly hand over the event without a custom event dispatcher
+		(component.$$.callbacks.keydown ?? [])
+			.slice()
+			.forEach((callback: (e: KeyboardEvent) => void) => callback(e));
+	}
+
+	/**
+	 * Checks if the icon slot contains only 1 element, which is an instance of SVGElement
+	 *
+	 * @param $icon the div element containing the icon slot
+	 */
+	function checkForSVG($icon: HTMLDivElement): ActionReturn {
+		$$slots.icon = $icon.childElementCount === 1 && $icon.firstChild instanceof SVGElement;
+
+		if ($$slots.icon === false) {
+			console.error(
+				`The icon slot for a Button component MUST contain a single SVG icon. Given content: ${$icon.innerHTML}`
+			);
+		}
+
+		return {};
+	}
+</script>
+
+<button
+	class={classNames(
+		'button',
+		className,
+		$$slots.icon ? `button--has-icon button--has-icon--${iconPosition}` : false,
+		hideText ? 'button--hide-text' : false
+	)}
+	{disabled}
+	on:click
+	on:focus
+	on:blur
+	on:keydown={normalizeKeydownEvent}
+	on:keyup
+	on:keypress
+	on:mouseover
+	on:mouseout
+	on:mouseenter
+	on:mouseleave
+>
+	{#if $$slots.icon}
+		<div class="button__icon" use:checkForSVG>
+			<slot name="icon" />
+		</div>
+	{/if}
+	{#if text.trim() !== ''}
+		<div class="button__text">{text}</div>
+	{/if}
+</button>
+
+<style lang="scss">
+	.button {
+		align-items: center;
+		cursor: pointer;
+		display: flex;
+		flex: {
+			direction: row;
+			wrap: nowrap;
+		}
+		justify-content: center;
+		padding: var(--button-padding);
+
+		&[disabled] {
+			cursor: not-allowed;
+		}
+
+		&__icon {
+			margin-right: var(--button-icon-margin);
+		}
+
+		&--has-icon--right {
+			flex-direction: row-reverse;
+		}
+
+		&--has-icon--right &__icon {
+			margin: {
+				right: 0;
+				left: var(--button-icon-margin);
+			}
+		}
+
+		&--hide-text &__text {
+			display: none;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import '$lib/styles/custom-properties.scss';
+	import '$lib/styles/normalize.scss';
+	import Button, { ButtonIconPosition } from '$lib/components/Button.svelte';
+</script>
+
+<dl>
+	<dt>button</dt>
+	<dd><Button text="test"></Button></dd>
+
+	<dt>button with classes</dt>
+	<dd><Button text="test" class={['test1', 'test2 test3', { test4: true }]}></Button></dd>
+
+	<dt>disabled button</dt>
+	<dd><Button text="test" disabled={true}></Button></dd>
+
+	<dt>button with click event</dt>
+	<dd><Button text="test" on:click={console.log}></Button></dd>
+
+	<dt>button with normalized onkeydown event</dt>
+	<dd><Button text="test" on:keydown={console.log}></Button></dd>
+
+	<dt>button with SVG icon left</dt>
+	<dd><Button text="test"><svg slot="icon"></svg></Button></dd>
+
+	<dt>button with SVG icon right</dt>
+	<dd>
+		<Button text="test" iconPosition={ButtonIconPosition.RIGHT}><svg slot="icon"></svg></Button>
+	</dd>
+
+	<dt>button with wrong icon</dt>
+	<dd><Button text="test"><div slot="icon"></div></Button></dd>
+
+	<dt>button with SVG icon left and hidden text</dt>
+	<dd><Button text="test" hideText={true}><svg slot="icon"></svg></Button></dd>
+</dl>
+
+<style lang="scss">
+	:global(body) {
+		font: {
+			family: Verdana, Helvetica, sans-serif;
+		}
+	}
+
+	dt,
+	dd {
+		margin: 1rem;
+	}
+
+	dt {
+		border-bottom: 1px solid black;
+	}
+
+	dd {
+		margin-bottom: 4rem;
+	}
+</style>
